abd52343f7a2949cf1e6d6c33f37e1397755db41
Weslin-0101 <wesleeylirac12@gmail.com>
2022-08-18 11:17:34 -0300

    feat: created DbAddAccount class and Encrypter interface

---
+++ src/data/useCases/add-account/db-add-account.ts
@@ -0,0 +1,18 @@
+import { AccountModel } from '../../../domain/models/account';
+import { AddAccount, AddAccountModel } from '../../../domain/useCases/addAccount'
+import { Encrypter } from '../../protocols/encrypter';
+
+export class DbAddAccount implements AddAccount {
+    private readonly _encrypter: Encrypter;
+
+    constructor(encrypter: Encrypter) {
+        this._encrypter = encrypter
+    }
+    
+    async add(account: AddAccountModel): Promise<AccountModel> {
+        await this._encrypter.encrypt(account.password)
+        
+        return new Promise(resolve => resolve(null))
+    }
+
+}
