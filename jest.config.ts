ddfaa4d914f4bcfa083267eb51b3d7666e882c89
Weslin-0101 <wesleeylirac12@gmail.com>
2022-08-28 10:48:48 -0300

    chore: refactor collectCoverageFrom

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,10 @@
 export default {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   preset: '@shelf/jest-mongodb',
