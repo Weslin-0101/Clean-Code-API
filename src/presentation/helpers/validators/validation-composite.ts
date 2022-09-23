df69840f1c4c532675c43f2da1029b82f022b22f
Weslin-0101 <wesleeylirac12@gmail.com>
2022-09-23 17:13:39 -0300

    refactor: move required fields validations to the ValidationComposite

---
+++ src/presentation/helpers/validators/validation-composite.ts
@@ -0,0 +1,18 @@
+import { Validation } from './validation'
+
+export class ValidationComposite implements Validation {
+    private readonly _validation: Validation[]
+    
+    constructor(validations: Validation[]) {
+        this._validation = validations
+    }
+    
+    validate(input: any): Error {
+        for (const validation of this._validation) {
+            const error = validation.validate(input)
+            if (error) {
+                return error
+            }
+        }
+    }
+}
