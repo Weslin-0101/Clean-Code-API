cebc4614f28f39923f92bbd723e41523739fbf05
Weslin-0101 <wesleeylirac12@gmail.com>
2022-09-23 10:23:27 -0300

    test: ensure LoginController calls EmailValidator with correct email

--- src/presentation/controller/login/login.spec.ts
+++ src/presentation/controller/login/login.spec.ts
@@ -1,15 +1,28 @@
 import { LoginController } from './login'
 import { badRequest } from '../../helpers/http-helper'
 import { MissingParamError } from '../../errors'
+import { EmailValidator } from '../signup/signup-protocols'
 
 interface SutTypes {
     sut: LoginController
+    emailValidatorStub: EmailValidator
+}
+
+const makeEmailValidator = (): EmailValidator => {
+    class EmailValidatorStub implements EmailValidator {
+        isValid(email: string): boolean {
+            return true
+        }
+    }
+    return new EmailValidatorStub()
 }
 
 const makeSut = (): SutTypes => {
-    const sut = new LoginController()
+    const emailValidatorStub = makeEmailValidator()
+    const sut = new LoginController(emailValidatorStub)
     return {
-        sut
+        sut,
+        emailValidatorStub
     }
 }
 
@@ -26,7 +39,7 @@ describe('Login Controller', () => {
     })
     
     test('Should return 400 if no password is provided', async () => {
-        const { sut} = makeSut()
+        const { sut } = makeSut()
         const httpRequest = {
             body: {
                 email: 'anyemail@email.com'
@@ -34,5 +47,18 @@ describe('Login Controller', () => {
         }
         const httpResponse = await sut.handle(httpRequest)
         expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+    })
+    
+    test('Should call EmailValidator with correct email', async () => {
+        const { sut, emailValidatorStub } = makeSut()
+        const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+        const httpRequest = {
+            body: {
+                email: 'anyemail@email.com',
+                password: 'any_password'
+            }
+        }
+        await sut.handle(httpRequest)
+        expect(isValidSpy).toHaveBeenCalledWith('anyemail@email.com')
     })
 })
