04ebba36fafab051880fd41eb6336a19b1383e1f
Weslin-0101 <wesleeylirac12@gmail.com>
2022-09-26 12:10:15 -0300

    feat: ensure AccountMongoRepository retuns an account on LoadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,14 +1,21 @@
 import { AddAccountRepository } from "../../../../data/protocols/db/add-account-repository";
+import { LoadAccountByEmailRepository } from "../../../../data/protocols/db/load-account-email-repository";
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/useCases/addAccount'
 import { MongoHelper } from '../helpes/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
     async add(accountData: AddAccountModel): Promise<AccountModel> {
         const accountCollection = await MongoHelper.getCollection('accounts')
         const result = await accountCollection.insertOne(accountData)
         const { insertedId: id } = result
         const accountById = await accountCollection.findOne({ _id: id })
         return MongoHelper.map(accountById)
+    }
+
+    async loadByEmail(email: string): Promise<AccountModel> {
+        const accountCollection = await MongoHelper.getCollection('accounts')
+        const account = await accountCollection.findOne({ email })
+        return account && MongoHelper.map(account)
     }
 }
