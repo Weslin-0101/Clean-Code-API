2874cb7dce1e44e5be2720aeb979863da93e2fef
Weslin-0101 <wesleeylirac12@gmail.com>
2022-09-26 11:21:11 -0300

    refactor: rename load method from LoadAccountEmailRepository to loadByEmail

--- src/data/useCases/authentication/db-authentication.ts
+++ src/data/useCases/authentication/db-authentication.ts
@@ -26,7 +26,7 @@ export class DbAuthentication implements Authentication {
     }
     
     async auth(authentication: AuthenticationModel): Promise<string> {
-        const account = await this._loadAccountByEmailRepository.load(authentication.email)
+        const account = await this._loadAccountByEmailRepository.loadByEmail(authentication.email)
         if (account) {
             const isValid = await this._hashComparer.compare(authentication.password, account.password)
             if (isValid) {
