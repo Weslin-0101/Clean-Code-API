18d85960e76df4f8bb40a0e4e2bb00cd6b66358e
Weslin-0101 <wesleeylirac12@gmail.com>
2022-09-26 12:11:55 -0300

    test: ensure AccountMongoRepository retuns null if LoadByEmail fails

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -46,4 +46,10 @@ describe('Account Mongo Repository', () => {
         expect(account.email).toBe('any_email@email.com')
         expect(account.password).toBe('any_password')
     })
+
+    test('Should return null if LoadByEmail fails', async () => {
+        const sut = new AccountMongoRepository()
+        const account = await sut.loadByEmail('any_email@email.com')
+        expect(account).toBeFalsy()
+    })
 })
