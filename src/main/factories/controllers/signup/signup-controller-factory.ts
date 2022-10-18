05cd8d4f22ea35d4b1a8325314636b18357f1932
Weslin-0101 <wesleeylirac12@gmail.com>
2022-10-18 10:55:06 -0300

    refactor: separate factories in folders

---
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -0,0 +1,11 @@
+import { SignUpController } from '../../../../presentation/controller/signup/signup-controller'
+import { Controller } from '../../../../presentation/protocols'
+import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
+import { makeDbAddAccount } from '../../useCases/add-account/add-account-factory'
+import { makeDbAuthentication } from '../../useCases/authentication/db-authentication-factory'
+import { makeSignUpValidation } from './signup-validation-factory'
+
+export const makeSignUpController = (): Controller => {
+    const controller = new SignUpController(makeDbAddAccount(), makeSignUpValidation(), makeDbAuthentication())
+    return makeLogControllerDecorator(controller)
+}
---
+++ src/main/factories/useCases/authentication/db-authentication-factory.ts
@@ -0,0 +1,15 @@
+import env from '../../../config/env'
+import { DbAuthentication } from "../../../../data/useCases/authentication/db-authentication";
+import { BcryptAdapter } from "../../../../infra/criptography/bcryot-adapter/bcrypt.adapter";
+import { JwtAdapter } from "../../../../infra/criptography/jwt-adapter/jwt-adapter";
+import { AccountMongoRepository } from "../../../../infra/db/mongodb/account/account-mongo-repository";
+import { Authentication } from '../../../../domain/useCases/authentication';
+
+export const makeDbAuthentication = (): Authentication => {
+    const salt = 12
+    
+    const jwtAdapter = new JwtAdapter(env.jwtSecret)
+    const bcryptAdapter = new BcryptAdapter(salt)
+    const accountMongoRepository = new AccountMongoRepository()
+    return new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+}
