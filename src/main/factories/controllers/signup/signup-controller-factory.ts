45ca17d7c9f10f46f1063735c871fc618549a91a
Weslin-0101 <wesleeylirac12@gmail.com>
2022-10-19 16:22:50 -0300

    refactor: moved login and signup controller to a folder

--- src/main/factories/controllers/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -1,4 +1,4 @@
-import { SignUpController } from '../../../../presentation/controller/signup/signup-controller'
+import { SignUpController } from '../../../../presentation/controller/login/signup/signup-controller'
 import { Controller } from '../../../../presentation/protocols'
 import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
 import { makeDbAddAccount } from '../../useCases/add-account/add-account-factory'
--- src/presentation/controller/login/login/login-controller.spec.ts
+++ src/presentation/controller/login/login/login-controller.spec.ts
@@ -1,9 +1,9 @@
 import { LoginController } from './login-controller'
-import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http-helper'
-import { MissingParamError } from '../../errors'
+import { badRequest, ok, serverError, unauthorized } from '../../../helpers/http/http-helper'
+import { MissingParamError } from '../../../errors'
 import { HttpRequest, Authentication } from './login-controller-protocols'
 import { Validation } from '../signup/signup-controller-protocols'
-import { AuthenticationModel } from '../../../domain/useCases/authentication'
+import { AuthenticationModel } from '../../../../domain/useCases/authentication'
 
 interface SutTypes {
     sut: LoginController
