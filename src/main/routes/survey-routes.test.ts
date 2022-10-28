47763dc057d387544e822e2c51e974978f80f4d2
Weslin-0101 <wesleeylirac12@gmail.com>
2022-10-28 10:58:51 -0300

    test: ensure add surveys route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,9 +2,11 @@ import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpes/mongo-helper'
 import app from '../config/app'
-import { hash } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
 
 describe('Survey Routes', () => {
     beforeAll(async () => {
@@ -18,6 +20,8 @@ describe('Survey Routes', () => {
     beforeEach(async () => {
         surveyCollection = await MongoHelper.getCollection('surveys')
         await surveyCollection.deleteMany({})
+        accountCollection = await MongoHelper.getCollection('accounts')
+        await accountCollection.deleteMany({})
     })
 
     describe('POST /surveys', () => {
@@ -34,6 +38,38 @@ describe('Survey Routes', () => {
                     }]
                 })
                 .expect(403)
+        })
+
+        test('Should return 204 on add survey with valid accessToken', async () => {
+            const res = await accountCollection.insertOne({
+                name: 'Lucas',
+                email: 'anyemail@email.com',
+                password: '123',
+                role: 'admin'
+            })
+
+            const id = res.insertedId
+            const accessToken = sign({ id }, env.jwtSecret)
+            await accountCollection.updateOne({
+                _id: id
+            }, {
+                $set: {
+                    accessToken
+                }
+            })
+            await request(app)
+                .post('/api/surveys')
+                .set('x-access-token', accessToken)
+                .send({
+                    question: 'Definir comida',
+                    answers: [{
+                        image: 'https://image.com',
+                        answer: 'Pizza'
+                    }, {
+                        answer: 'Lasanha'
+                    }]
+                })
+                .expect(204)
         })
     })
 })
