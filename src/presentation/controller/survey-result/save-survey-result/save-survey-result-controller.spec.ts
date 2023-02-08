44d1ca32dfb83b8bd9d70e28b3245682146f73fa
Weslin-0101 <wesleeylirac12@gmail.com>
2023-02-08 12:33:58 -0300

    test: ensure SaveSurveyResultController returns 200 on success

--- src/presentation/controller/survey-result/save-survey-result/save-survey-result-controller.spec.ts
+++ src/presentation/controller/survey-result/save-survey-result/save-survey-result-controller.spec.ts
@@ -1,6 +1,7 @@
 import { InvalidParamError } from "@/presentation/errors";
 import {
   forbidden,
+  ok,
   serverError,
 } from "@/presentation/helpers/http/http-helper";
 import { SaveSurveyResultController } from "./save-survey-result-controller";
@@ -156,4 +157,10 @@ describe("SaveSurveyResult Controller", () => {
   //     const httpResponse = await sut.handle({});
   //     expect(httpResponse).toEqual(serverError(new Error()));
   //   });
+
+  test("Should return 200 on success", async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(ok(makeFakeSurveyResult()));
+  });
 });
