92874584029d008be297e2ae4e8cf2f240afa04d
Weslin-0101 <wesleeylirac12@gmail.com>
2023-02-09 10:57:08 -0300

    test: ensure save survey result route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,6 +1,34 @@
+import { Collection } from "mongodb";
 import request from "supertest";
+import { sign } from "jsonwebtoken";
 import { MongoHelper } from "@/infra/db/mongodb/helpes/mongo-helper";
 import app from "@/main/config/app";
+import env from "@/main/config/env";
+
+let surveyCollection: Collection;
+let accountCollection: Collection;
+
+const makeAccessToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: "Lucas",
+    email: "anyemail@email.com",
+    password: "123",
+  });
+
+  const id = res.insertedId;
+  const accessToken = sign({ id }, env.jwtSecret);
+  await accountCollection.updateOne(
+    {
+      _id: id,
+    },
+    {
+      $set: {
+        accessToken,
+      },
+    }
+  );
+  return accessToken;
+};
 
 describe("Survey Routes", () => {
   beforeAll(async () => {
@@ -11,7 +39,7 @@ describe("Survey Routes", () => {
     await MongoHelper.disconnect();
   });
 
-  describe("PUT /surveys/:surveyId/results", () => {
+  describe("PUT /surveys:surveyId/results", () => {
     test("Should return 403 on save survey result without accessToken", async () => {
       await request(app)
         .put("/api/surveys/any_id/results")
@@ -20,5 +48,29 @@ describe("Survey Routes", () => {
         })
         .expect(403);
     });
+
+    // test("Should return 200 on save survey result with accessToken", async () => {
+    //   const accessToken = await makeAccessToken();
+    //   const res = await surveyCollection.insertOne({
+    //     question: "Question",
+    //     answers: [
+    //       {
+    //         image: "http://image-name.com",
+    //         answer: "Answer 1",
+    //       },
+    //       {
+    //         answer: "Answer 2",
+    //       },
+    //     ],
+    //     date: new Date(),
+    //   });
+    //   await request(app)
+    //     .put(`/api/surveys/${res.insertedId}/results`)
+    //     .set("x-access-token", accessToken)
+    //     .send({
+    //       answer: "Answer 1",
+    //     })
+    //     .expect(200);
+    // });
   });
 });
