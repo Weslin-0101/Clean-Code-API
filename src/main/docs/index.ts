85efdb80abe6d9417df0d4d9ac28e2c91dd3b600
Weslin-0101 <wesleeylirac12@gmail.com>
2023-02-12 17:14:58 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,3 +1,7 @@
+import { loginPath } from "@/main/docs/paths/login-path";
+import { accountSchema } from "@/main/docs/schemas/account-schema";
+import { loginParamsSchema } from "@/main/docs/schemas/login-params-schema";
+
 export default {
   openapi: "3.0.0",
   info: {
@@ -5,5 +9,22 @@ export default {
     description:
       "API do curso de NodeJs, Typescript, TDD, clean architecture e SOLID",
     version: "1.0.0",
+  },
+  servers: [
+    {
+      url: "/api",
+    },
+  ],
+  tags: [
+    {
+      name: "Login",
+    },
+  ],
+  paths: {
+    "/login": loginPath,
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema,
   },
 };
