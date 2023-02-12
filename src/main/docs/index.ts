99472b1b8e08f6c1e9401f984652cdaa001a4504
Weslin-0101 <wesleeylirac12@gmail.com>
2023-02-12 16:41:54 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,9 @@
+export default {
+  openapi: "3.0.0",
+  info: {
+    title: "Clean Node API",
+    description:
+      "API do curso de NodeJs, Typescript, TDD, clean architecture e SOLID",
+    version: "1.0.0",
+  },
+};
