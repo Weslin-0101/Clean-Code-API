217ebaab33bf2d868eef557ed373170a8854484a
Weslin-0101 <wesleeylirac12@gmail.com>
2023-02-17 15:22:15 -0300

    fix: change DbLoadAccountByToken to return null if Decrypter throws

--- src/data/useCases/account/load-account-by-token/db-load-account-by-token.ts
+++ src/data/useCases/account/load-account-by-token/db-load-account-by-token.ts
@@ -1,16 +1,31 @@
-import { LoadAccountByToken, Decrypter, LoadAccountByTokenRepository, AccountModel } from "./db-load-account-by-token-protocols";
+import {
+  LoadAccountByToken,
+  Decrypter,
+  LoadAccountByTokenRepository,
+  AccountModel,
+} from "./db-load-account-by-token-protocols";
 
 export class DbLoadAccountByToken implements LoadAccountByToken {
-    constructor(
-        private readonly _decrypter: Decrypter,
-        private readonly _loadAccountByTokenRepository: LoadAccountByTokenRepository) {}
-    
-    async load(accessToken: string, role?: string): Promise<AccountModel> {
-        const token = await this._decrypter.decrypt(accessToken)
-        if (token) {
-            const account = await this._loadAccountByTokenRepository.loadByToken(accessToken, role)
-            if (account) return account
-        }
-        return null
+  constructor(
+    private readonly _decrypter: Decrypter,
+    private readonly _loadAccountByTokenRepository: LoadAccountByTokenRepository
+  ) {}
+
+  async load(accessToken: string, role?: string): Promise<AccountModel> {
+    let token: string;
+    try {
+      token = await this._decrypter.decrypt(accessToken);
+    } catch (error) {
+      return null;
     }
+
+    if (token) {
+      const account = await this._loadAccountByTokenRepository.loadByToken(
+        accessToken,
+        role
+      );
+      if (account) return account;
+    }
+    return null;
+  }
 }
