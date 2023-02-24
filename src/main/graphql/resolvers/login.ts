bae9bd88c12b7425004ab984dc7afed866675c2e
Weslin-0101 <wesleeylirac12@gmail.com>
2023-02-24 10:28:32 -0300

    feat: make login resolver use login controller

--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,10 +1,9 @@
+import { adpatResolver } from "@/main/adapters/express/apollo-server-resolver-adapter";
+import { makeLoginController } from "@/main/factories/controllers/login/login-controller-factory";
+
 export default {
   Query: {
-    login() {
-      return {
-        acessToken: "any_token",
-        name: "any_name",
-      };
-    },
+    login: async (parent: any, args: any) =>
+      adpatResolver(makeLoginController(), args),
   },
 };
